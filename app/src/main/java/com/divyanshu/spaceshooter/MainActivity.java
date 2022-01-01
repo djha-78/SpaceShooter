245ed7411e55929551b49b77b187844981327588
contactbj9 <contactbj969@gmail.com>
2022-01-01 21:59:26 +0530

    removed facebook ads

--- app/src/main/java/com/divyanshu/spaceshooter/MainActivity.java
+++ app/src/main/java/com/divyanshu/spaceshooter/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(new SpaceShooter(this));
+
     }
 
     @Override
